340d7dda1c6fcd2ef69de3b0821dda29e62d4689
jacob-israel-turner <poppa.bear.joe@gmail.com>
2015-01-24 21:21:07 -0700

    :boar: debugging.

--- src/js/constants/AppConstants.js
+++ src/js/constants/AppConstants.js
@@ -3,7 +3,7 @@ var AppConstants = {
   REMOVE_USER: 'REMOVE_USER',
   INITIALIZE_USER:'INITIALIZE_USER',
   UPDATE_USER: "UPDATE_USER",
-  FIREBASE_HOST: 'https://edq.firebaseio.com/',
+  FIREBASE_HOST: 'https://edq.firebaseio.com/'
 };
 
 module.exports = AppConstants;
