70557a9fca129b487a0bff5374c77f91d4489be6
jacob-israel-turner <poppa.bear.joe@gmail.com>
2015-01-24 20:03:14 -0700

    :boar: adds fontawesome.  Works on Settings page.

--- src/js/components/UserSettings/Settings.js
+++ src/js/components/UserSettings/Settings.js
@@ -7,44 +7,77 @@ var Settings = React.createClass({
 	getInitialState: function(){
 		return {
 			user: UserStore.getState(),
-			hideNormal: false
+			showNameInput: false,
+			showPassInput: false,
+			newName: '',
+			originalPass: '',
+			newPass: ''
 		}
 	},
-	handleChangePassword: function(){
-
-	},
-	handleEditToggle: function(){
+	handleNameToggle: function(){
 		this.setState({
-			hideNormal: !this.state.hideNormal
+			showNameInput: !this.state.showNameInput
 		})
+	},
+	handlePasswordToggle: function(){
+		this.setState({
+			showPassInput: !this.state.showPassInput
+		})
+	},
+	handleChange: function(prop, e){
+		var obj = {};
+		obj[prop] = e.target.value;
+		this.setState(obj)
+	},
+	handleChangePassword: function(){
+		var obj = {
+			old: this.state.originalPass,
+			new: this.state.newPass
+		};
+		console.log(obj);
+	},
+	handleChangeName: function(){
+		console.log(this.state.newName);
 	},
 	render: function(){
 		var cx = ReactAddons.addons.classSet;
-		var normalClasses = cx({
-			hide: this.state.hideNormal
+		var normalClass = cx({
+			hide: this.state.showNameInput
 		});
-		var inputClasses = cx({
-			hide: !this.state.hideNormal,
-			'form-control': true
+		var nameInputClass = cx({
+			hide: !this.state.showNameInput,
+			'input-group': true
+		});
+		var passwordInputClass = cx({
+			hide: !this.state.showPassInput,
+			'input-group': true
 		});
 		return (
 			<div className='col-md-6 col-md-offset-3'>
 				<div className='panel panel-default'>
 					<div className='panel-heading'>
 						<div><h1>Profile</h1></div>
-						<div><button className='btn btn-default' onClick={this.handleEditToggle}>EDIT</button></div>
 					</div>
 				<div className='panel-body'>
 					<div className='form-group'>
-						<label>Name:</label>
-						<div className={normalClasses}>ME{this.state.user.name}</div><input type='text'className={inputClasses}/>
+						<i onClick={this.handleNameToggle} className="fa fa-cog"></i><label>Name:</label>
+						<div className={normalClass}>{this.state.user.name}</div>
+						<div className={nameInputClass} >
+							<input type='text' className='form-control'value={this.state.newName} onChange={this.handleChange.bind(this, 'newName')}/>
+							<button className='btn btn-default' onClick={this.handleChangeName} >Submit</button>
+						</div>
 					</div>
 					<div className='form-group'>
 						<label>Email:</label>
-						<div className={normalClasses}>MYEMAIL{this.state.user.email}</div><input type='text' className={inputClasses} />
+						<div >{this.state.user.email}</div>
 					</div>
-					<button className='btn btn-default' onClick={this.handleChangePassword}>Change Password</button>
+					<button className='btn btn-default' onClick={this.handlePasswordToggle}>Change Password</button>
+					<div className={passwordInputClass}>
+						<input placeholder='Old Password' type='password' className='form-control' value={this.state.originalPass} onChange={this.handleChange.bind(this, 'originalPass')}/>
+						<input placeholder='New Password' type='password' className='form-control' value={this.state.newPass} onChange={this.handleChange.bind(this, 'newPass')}/>
+						<button className='btn btn-default' onClick={this.handleChangePassword} >Submit</button>
 					</div>
+				</div>
 				</div>
 			</div>
 		)
